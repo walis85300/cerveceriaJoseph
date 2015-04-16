e08aff8774346a6102f5ea165492050ea8c95e7f
Kevin Hernández <kevteg@laboratorio-prototipos.(none)>
2015-04-16 15:08:33 -0430

    creo nque se solucoino

--- server.js
+++ server.js
@@ -7,36 +7,7 @@ var handlebars = require('express-handlebars')
 app.use(express.static(__dirname + '/public'));
 
 require('./expressHandlers/setters.js')(app, handlebars);
-// require('./expressHandlers/handlingRoutes.js')(app);
-
-app.get('/', function(res, res){
-	res.render('home');
-});
-
-app.get('/simulation', function(req, res){
-	res.render('simulation');
-});
-
-app.get('/about', function(req, res){
-	res.render('about');
-});
-
-app.get('/test', function(req, res){
-	res.render('test');
-});
-
-app.route('/api/:led/:pos').get(function(req, res, next){
-	console.log(req.params.led);
-	console.log(req.params.pos);
-
-	res.sendStatus(200);
-});
-
-app.use(function(req, res, next){
-	res.status(404);
-	res.render('404');
-});
-
+require('./expressHandlers/handlingRoutes.js')(app);
 
 
 app.listen(app.get('port'), function(){
