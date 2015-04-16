ffac02c7bfdc53ff75577e21e00ed95560bfb052
Kevin Hernández <kevteg@laboratorio-prototipos.(none)>
2015-04-16 15:06:32 -0430

    yyyy

--- expressHandlers/handlingRoutes.js
+++ expressHandlers/handlingRoutes.js
@@ -1,33 +1,33 @@
 
 module.exports = function(app){
 
-    app.get('/', function(res, res){
-        res.render('home');
-    });
-
-    app.get('/simulation', function(req, res){
-        res.render('simulation');
-    });
-
-    app.get('/about', function(req, res){
-        res.render('about');
-    });
-
-    app.get('/test', function(req, res){
-        res.render('test');
-    });
-
-    app.use(function(req, res, next){
-        res.status(404);
-        res.render('404');
-    });
-
-    app.route('/api/:led/:pos').get(function(req, res, next){
-        console.log(req.params.led);
-        console.log(req.params.pos);
-
-        res.sendStatus(200);
-    });
+    // app.get('/', function(res, res){
+    //     res.render('home');
+    // });
+    //
+    // app.get('/simulation', function(req, res){
+    //     res.render('simulation');
+    // });
+    //
+    // app.get('/about', function(req, res){
+    //     res.render('about');
+    // });
+    //
+    // app.get('/test', function(req, res){
+    //     res.render('test');
+    // });
+    //
+    // app.use(function(req, res, next){
+    //     res.status(404);
+    //     res.render('404');
+    // });
+    //
+    // app.route('/api/:led/:pos').get(function(req, res, next){
+    //     console.log(req.params.led);
+    //     console.log(req.params.pos);
+    //
+    //     res.sendStatus(200);
+    // });
 
     // require('../ledControl/led.js')(app);
 
