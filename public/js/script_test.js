19485319bc78a867dc41251a078a4d2c681dd6af
Kevin Hernández <kevin Hernandez@unet.edu.ve>
2015-04-15 22:23:24 -0430

    Se agrega script_test.js

---
+++ public/js/script_test.js
@@ -0,0 +1,28 @@
+$('.btn').on('click', function(){
+
+    var led = $(this).attr('id');
+
+    if ($(this).hasClass('pressed')) {
+
+        $(this).removeClass('pressed')
+            .removeClass('btn-default')
+            .addClass('btn-primary');
+
+        $.get('/api/' + led + '/off', function(data){
+            toastr["success"](led + ' on ' + data);
+        });
+
+    } else {
+
+        $(this).addClass('pressed')
+            .removeClass('btn-primary')
+            .addClass('btn-default');
+
+        $.get('/api/' + led + '/on', function(data){
+            toastr["success"](led + ' off ' + data);
+        });
+
+        console.log('encendio ' + $(this).attr('id'));
+    }
+
+});
